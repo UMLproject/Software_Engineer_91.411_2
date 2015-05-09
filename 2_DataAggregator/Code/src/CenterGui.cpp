5a04fa8e5fde24ce52cc8da9852cc7c6b53e9205
DeepBlue14 <james.perl12@gmail.com>
2015-05-09 16:00:08 -0400

    Additional bug fixes

--- 2_DataAggregator/Code/src/CenterGui.cpp
+++ 2_DataAggregator/Code/src/CenterGui.cpp
@@ -2,14 +2,21 @@
 
 CenterGui::CenterGui(QWidget* parent) : QWidget(parent)
 {
-    usernameLbl = new QLabel("<qt>Welcome, <b>username_here</b></qt>");
-    lastLoginLbl = new QLabel("Last login: last_login_date_here");
-    exercisesLbl = new QLabel("Exercises: \n\t- bullet\n\t- points\n\t- containing\n\t- exercises");
+    usernameLbl = new QLabel("Welcome!");
+    
+    QFont* font1 = new QFont("Serif");
+    font1->setBold(true);
+    font1->setPointSize(14);
+    usernameLbl->setFont(*font1);
+    
+    QFont* font2 = new QFont("Serif");
+    font2->setPointSize(12);
+    textLbl = new QLabel("Welcome to the dashboard.\nPlease select an optionfrom\nthe menu to the right ->");
+    textLbl->setFont(*font2);
     
     outerLayout = new QGridLayout(this);
     outerLayout->addWidget(usernameLbl, 0, 0, Qt::AlignCenter);
-    outerLayout->addWidget(lastLoginLbl, 1, 0, Qt::AlignHCenter);
-    outerLayout->addWidget(exercisesLbl, 2, 0, Qt::AlignHCenter);
+    outerLayout->addWidget(textLbl, 1, 0, Qt::AlignHCenter);
     
     this->setLayout(outerLayout);
     this->setMinimumSize(500, 300);//setMinimumSize(600, 400);
